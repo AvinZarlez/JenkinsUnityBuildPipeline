836e93e4de0a0a6ae90da6b8913b6ee49bcd8a30
Tobiah Zarlez <TobiahZ@users.noreply.github.com>
2016-09-12 15:15:08 -0700

    first commit

---
+++ Assets/BallMoveScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMoveScript : MonoBehaviour {
+
+    bool goingNorth = true;
+    bool goingWest = true;
+
+    public float speed;
+    public float distance;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((transform.position.x <= -distance) || (transform.position.x >= distance)) goingWest = !goingWest;
+        if ((transform.position.y <= -distance) || (transform.position.y >= distance)) goingNorth = !goingNorth;
+
+        Vector3 goal = new Vector3(distance,-distance, 0);
+
+        if (goingNorth) goal.y =  distance;
+        if (goingWest)  goal.x = -distance;
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, goal, step);
+
+    }
+}

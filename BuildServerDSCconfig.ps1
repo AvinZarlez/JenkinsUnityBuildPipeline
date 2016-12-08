c7884a8fb60fd53379a2ab12d81608850c14cfce
Jennelle Crothers <jkcrothers@hotmail.com>
2016-12-08 14:03:23 -0800

    added parameters file and DSC config

---
+++ BuildServerDSCconfig.ps1
@@ -0,0 +1,33 @@
+﻿Configuration BuildServer            
+{            
+    Import-DscResource -ModuleName PSDesiredStateConfiguration, cChoco, xSystemSecurity          
+    Node localhost            
+    {       
+        WindowsFeature NETFramework35
+        {
+        Ensure = "Present"
+        Name = "NET-Framework-Features"
+    }
+       cChocoInstaller installChoco
+      {
+        InstallDir = "C:\Program Files (x86)\choco"
+      }
+      cChocoPackageInstaller installGit
+      {
+         Ensure = 'Present'
+         Name = "git"
+         DependsOn = "[cChocoInstaller]installChoco"
+      }
+      xIEEsc EnableIEEscAdmin
+        {
+            IsEnabled = $True
+            UserRole  = "Administrators"
+        }
+        xIEEsc EnableIEEscUser
+        {
+            IsEnabled = $False
+            UserRole  = "Users"
+        }
+
+    }
+}
---
+++ StartHere.ps1
@@ -0,0 +1,38 @@
+## Use this file to start the deployment of your Jenkins Pipeline Build Server ##
+
+# Step 1 #
+# Copy and edit the "buildserver.paramenters.json" file to have names globally unique names for your deployment
+# Save the file to a convienent location on your local machine
+
+# Step 2 #
+# Connect to Your Azure Subscription and create required resources #
+
+Login-AzureRmAccount
+Get-AzureRmSubscription
+$VSSubID = "8bae2860-70c1-4614-xxxx-60e97f4248dc"
+Set-AzureRmContext -SubscriptionID $VSSubID
+
+$RGName = "BuildServer"   
+New-AzureRmResourceGroup -Name $RGName -Location "West US" 
+
+# Step 3 #
+# Set the variable to deploy the server template #
+# You will be deploying directly from Github - https://github.com/TobiahZ/JenkinsUnityBuildPipeline/blob/master/buildserverdeploy.json""
+
+$assetLocation = "https://github.com/TobiahZ/JenkinsUnityBuildPipeline/blob/master/" 
+$templateFileURI  = $assetLocation + "buildserverdeploy.json" 
+$parameterFile = "C:\users\jcroth\Desktop\jennelle.parameters.json"
+
+# Step 4 #
+# Deploy the build server to your Azure Subscription #
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -TemplateParameterFile $parameterFile -TemplateUri $templateFileURI -verbose
+
+
+
+#Copy required DSC resources to local machine
+
+Save-Module -Name cChoco -Path 'C:\Program Files (x86)'
+Install-Module -Name cChoco
+
+

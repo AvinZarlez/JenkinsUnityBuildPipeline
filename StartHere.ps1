2349cf53a3634a626dce3907626616d1a7a99b0a
Jennelle Crothers <jkcrothers@hotmail.com>
2016-12-07 16:06:29 -0800

    Revert "Removed Custom Extension"
    This reverts commit 55904e738b3df80bee4123730a54c027a12b85cf.

--- StartHere.ps1
+++
@@ -1,9 +0,0 @@
-#Use this file to start the deployment of your Jenkins Pipeline Build Server
-
-#Copy required DSC resources to local machine
-
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Save-Module -Name cChoco -Path 'C:\Program Files (x86)'
-Install-Module -Name cChoco
-
-

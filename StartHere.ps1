8723ed3dec5344303a9fb0fe30943af282b7f940
Jennelle Crothers <jkcrothers@hotmail.com>
2016-12-14 13:51:13 -0800

    Edits to deployment

--- StartHere.ps1
+++ StartHere.ps1
@@ -1,30 +1,33 @@
 ## Use this file to start the deployment of your Jenkins Pipeline Build Server ##
+## Without modification, this deployment will build:           ##
+##              Windows Server 2012 R2 with:                   ##
+##                   Visual Studio Community - 2015 Update 3   ##
+##                   Jenkins 2.19.4                            ##
+##                   Unity 5.4.1                               ##
+##                   Git 2.11.0                                ##
+
+
 
 # Step 1 #
-# Copy and edit the "buildserver.paramenters.json" file to have names globally unique names for your deployment
-# Save the file to a convienent location on your local machine
-
-# Step 2 #
 # Connect to Your Azure Subscription and create required resources #
 
 Login-AzureRmAccount
 Get-AzureRmSubscription
-$VSSubID = "8bae2860-70c1-4614-xxxx-60e97f4248dc"
+$VSSubID = "8bae2860-xxxx-4614-xxxx-60e97f4248dc"  ## replace with your Sub ID ##
 Set-AzureRmContext -SubscriptionID $VSSubID
 
-$RGName = "BuildServer6"   
+$RGName = "BuildServerXXX"   
 New-AzureRmResourceGroup -Name $RGName -Location "West US" 
 
-# Step 3 #
+# Step 2 #
 # Set the variable to deploy the server template #
-# You will be deploying directly from Github - https://github.com/TobiahZ/JenkinsUnityBuildPipeline/blob/master/buildserverdeploy.json""
+# You will be deploying directly from Github - https://github.com/TobiahZ/JenkinsUnityBuildPipeline/blob/master/buildserverdeploy.json"
 
-$assetLocation = "https://raw.githubusercontent.com/TobiahZ/JenkinsUnityBuildPipeline/InfraDev/" 
+$assetLocation = "https://raw.githubusercontent.com/TobiahZ/JenkinsUnityBuildPipeline/master/" 
 $templateFileLoc  = $assetLocation + "buildserverdeploy.json" 
 
-# Step 4 #
+# Step 3 #
 # Deploy the build server to your Azure Subscription #
 
 New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName -TemplateUri $templateFileLoc -verbose
-
 

bc8cc0a4aff8acc862243c2a4bbecfaf3e8bb49b
Jennelle Crothers <jkcrothers@hotmail.com>
2017-01-06 10:33:12 -0800

    Added instructions

--- StartHere.ps1
+++ StartHere.ps1
@@ -19,9 +19,16 @@ Set-AzureRmContext -SubscriptionID $VSSubID
 $RGName = "BuildServerXXX"   
 New-AzureRmResourceGroup -Name $RGName -Location "West US" 
 
+# Step 1.5 #
+# If you need to edit the default installation of software from what is listed above, you will need to fork the GitHub repo and regenerate the DSC .zip file.
+# Edit the "BuildServerDSCconfig.ps1" file to include what software you need, then regenerate the zip file using the PowerShell below. 
+
+Publish-AzureRmVMDscConfiguration "C:\Users\jcroth\Documents\GitHub\JenkinsUnityBuildPipeline\BuildServerDSCconfig.ps1" -OutputArchivePath "C:\Users\jcroth\Documents\GitHub\JenkinsUnityBuildPipeline\BuildServerDSCconfig.ps1.zip" -Force
+
 # Step 2 #
 # Set the variable to deploy the server template #
-# You will be deploying directly from Github - https://github.com/TobiahZ/JenkinsUnityBuildPipeline/blob/master/buildserverdeploy.json"
+# You will be deploying directly from Github, this URL leads to our default installation - https://github.com/TobiahZ/JenkinsUnityBuildPipeline/blob/master/buildserverdeploy.json"
+# Otherwise, use a URL for your own forked repo.
 
 $assetLocation = "https://raw.githubusercontent.com/TobiahZ/JenkinsUnityBuildPipeline/master/" 
 $templateFileLoc  = $assetLocation + "buildserverdeploy.json" 
